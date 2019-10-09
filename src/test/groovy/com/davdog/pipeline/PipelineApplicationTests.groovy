825e5311b82e3d666db57a6ebd4a066bb555f79d
Davin Pidoto <davdog@me.com>
2019-10-09 17:43:10 +0200

    add repo call

--- src/test/groovy/com/davdog/pipeline/PipelineApplicationTests.groovy
+++ src/test/groovy/com/davdog/pipeline/PipelineApplicationTests.groovy
@@ -1,8 +1,10 @@
 package com.davdog.pipeline
 
+import com.davdog.pipeline.repository.BookRepository
 import org.assertj.core.api.Assertions
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
 
@@ -10,8 +12,12 @@ import org.springframework.test.context.junit4.SpringRunner
 @SpringBootTest
 class PipelineApplicationTests {
 
+	@Autowired
+	BookRepository bookRepository
+
 	@Test
 	void contextLoads() {
+		bookRepository.findAll()
 		Assertions.assertThat(1).isEqualTo(1)
 	}
 
